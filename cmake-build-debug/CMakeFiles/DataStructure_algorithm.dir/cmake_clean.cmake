18fc2653d46e567a0150dc5c92566aa3a97b403b
VisualDust <me@VisualDust.com>
2020-11-10 12:11:38 +0800

    add notes

--- cmake-build-debug/CMakeFiles/DataStructure_algorithm.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/DataStructure_algorithm.dir/cmake_clean.cmake
@@ -1,5 +1,6 @@
 file(REMOVE_RECURSE
   "CMakeFiles/DataStructure_algorithm.dir/experiment01/test.c.obj"
+  "CMakeFiles/DataStructure_algorithm.dir/experiment06/HaffmanT.cpp.obj"
   "DataStructure_algorithm.exe"
   "DataStructure_algorithm.exe.manifest"
   "DataStructure_algorithm.pdb"
@@ -7,6 +8,6 @@ file(REMOVE_RECURSE
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang C)
+foreach(lang C CXX)
   include(CMakeFiles/DataStructure_algorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
---
+++ experiment06/HaffmanT.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+struct node {
+    int data;
+    struct node* lchild;
+    struct node* rchild;
+};
+
